700b37db03b0fe9605f21addc7adf8b96d63dbb6
Jamie Bull <jamiebull27@gmail.com>
2020-04-08 00:11:50 +0100

    [JB] Initial commit

---
+++ src/main/java/org/github/jamiebull27/model/Planet.java
@@ -0,0 +1,7 @@
+package org.github.jamiebull27.model;
+
+import java.math.BigDecimal;
+
+public class Planet {
+
+}
---
+++ src/test/java/org/github/jamiebull27/model/PlanetTest.java
@@ -0,0 +1,13 @@
+package org.github.jamiebull27.model;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class PlanetTest {
+
+    private Planet planet;
+
+    @BeforeEach
+    public void setUp(){
+        this.planet = new Planet();
+    }
+}
